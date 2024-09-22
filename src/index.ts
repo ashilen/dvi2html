c19d3c1404b3963b6b488b327b8187d894068b36
Alex Shilen <ashilen1@jh.edu>
2024-09-22 09:18:24 -0400

    fix: drop a few fonts; remove import of missing dep

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,14 @@ import svg from "./specials/svg";
 import html from "./specials/html";
 import papersize from "./specials/papersize";
 import title from "./specials/title";
-import ximera from "./specials/ximera";
 
-import VDomMachine from "./vdom";
 import HTMLMachine from "./html";
 import TextMachine from "./text";
 
-export var Machines = { HTML: HTMLMachine,
-                        vdom: VDomMachine,
-			text: TextMachine };
+export var Machines = {
+  HTML: HTMLMachine,
+  text: TextMachine
+};
 
 import { dviParser, execute, mergeText } from "./parser";
 
@@ -22,31 +21,12 @@ export var specials = {
   papersize: papersize
 };
 
-export function dvi2html( dviStream, htmlStream ) {
+export function dvi2html(dviStream, htmlStream) {
   let parser = title(papersize(html(svg(color(mergeText(dviParser(dviStream)))))));
 
-  let machine = new HTMLMachine( htmlStream );
+  let machine = new HTMLMachine(htmlStream);
 
-  execute( parser, machine );
-
-  return machine;
-}
-
-export function dvi2vdom( dviStream: any,
-                          h: any,
-                          ximeraRuleHandler : any,
-                          ximeraPushHandler : any,
-                          ximeraPopHandler : any,
-                          callback: any ) {
-  let parser = ximera(title(papersize(html(svg(color(mergeText(dviParser(dviStream))))))));
-
-  let machine = new VDomMachine( h,
-                                 ximeraRuleHandler,
-                                 ximeraPushHandler,
-                                 ximeraPopHandler,
-                                 callback );
-
-  execute( parser, machine );
+  execute(parser, machine);
 
   return machine;
 }
